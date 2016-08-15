4e93dd81699f60317ef01c69061caecb1353b9b0
ediber <edibersh@gmail.com>
2016-08-15 16:09:36 +0300

    a

---
+++ app/src/main/java/edi/com/myrx2/MainActivity.java
@@ -0,0 +1,81 @@
+package edi.com.myrx2;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // 1
+        /*Observable<String> myObservable = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> sub) {
+                        sub.onNext("Hello, world!");
+                        sub.onCompleted();
+                    }
+                }
+        );
+
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d("mySubscriber", s);
+            }
+
+            @Override
+            public void onCompleted() { }
+
+            @Override
+            public void onError(Throwable e) { }
+        };
+
+        myObservable.subscribe(mySubscriber);*/
+
+
+        //2
+        Observable<String> myObservable =
+                Observable.just("Hello, world!");
+
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        };
+
+        myObservable.subscribe(onNextAction);
+// Outputs "Hello, world!"
+
+
+        //3
+        Observable.just("Hello, world!")
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+//                        System.out.println(s);
+                    }
+                });
+
+        // 4
+        Observable.just("Hello, world!")
+                .subscribe(s ->
+                        System.out.println(s)
+                );
+    }
+
+
+}
